3629d8f9a85709c9eda69245c6fb9d848317a24d
Nur A Alam <nuraalam376@gmail.com>
2020-07-27 16:32:59 +0600

    Handle user input  number

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
+let userInputNumber = "";
+
 // Generates the 4 digit pin and set the pin in the input
 function generatePin() {
 	const pin = Math.round(Math.random() * 9000 + 1000);
 	document.getElementById("pin").value = pin;
+}
+
+// Handles the number user enters
+function handleUserInput(number) {
+	//Checks whether the user inputs only 4 characters
+	if (userInputNumber.length >= 4) {
+		return;
+	}
+	// Add previous number with the newest and set the user number
+	userInputNumber += number.toString();
+	document.getElementById("userNumber").value = userInputNumber;
 }
